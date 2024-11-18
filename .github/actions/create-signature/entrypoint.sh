f34bc085e5e2dd49f43cb7957f643235abab7e7e
lesnerd <lesnerd@gmail.com>
2024-11-18 18:23:31 +0200

    Fix solution

--- .github/actions/create-signature/entrypoint.sh
+++ .github/actions/create-signature/entrypoint.sh
@@ -7,6 +7,7 @@ set -e
 # Capture input parameters
 PACKAGE_NAME=$1
 PACKAGE_REPO_NAME=$2
+PRIVATE_KEY=$3
 
 # Set package version from GitHub run number
 PACKAGE_VERSION="${RUN_NUMBER}"
@@ -16,5 +17,5 @@ echo '{ "actor": "'"${GITHUB_ACTOR}"'", "date": "'$(date -u +"%Y-%m-%dT%H:%M:%SZ
 
 # Run the jf evd command
 jf evd create --package-name "$PACKAGE_NAME" --package-version "$PACKAGE_VERSION" --package-repo-name "$PACKAGE_REPO_NAME" \
-  --key "${PRIVATE_KEY}" \
+  --key "$PRIVATE_KEY" \
   --predicate ./sign.json --predicate-type https://jfrog.com/evidence/signature/v1
